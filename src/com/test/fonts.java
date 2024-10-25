6ce5c8ed659df992b85c044cd3fb2449d1c66c8f
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-10-25 14:30:36 +0530

    Update fonts.java

---
+++ src/com/test/fonts.java
@@ -0,0 +1,21 @@
+package com.test;
+import java.awt.*;
+
+
+public class fonts {
+    public static void main(String[] args)
+    {
+        System.out.println("To Know the available font family names");
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+
+        System.out.println("Getting the font family names");
+
+        // Array of all the fonts available in AWT
+        String fonts[] = ge.getAvailableFontFamilyNames();
+
+        // Getting the font family names
+        for (String i : fonts) {
+            System.out.println(i + " ");
+        }
+    }
+}
